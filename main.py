f8a372958536065139f154c577b333b5a254aa66
Peter Holdsworth <peter.holdsworth@and.digital>
2023-10-24 16:28:13 +0100

    post with return of dummy results

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.post("/what_language_is_this")
 async def root():
-    return {"message": "Hello World"}
+    return {"predictions":[
+        {"class":"German","value":0.1},
+        {"class":"Esperanto","value":0.1},
+        {"class":"French","value":0.1},
+        {"class":"Italian","value":0.4},
+        {"class":"Spanish","value":0.1},
+        {"class":"Turkish","value":0.1},
+        {"class":"English","value":0.1}
+    ]}
